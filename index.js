7c3592b73a63c73dd84a451029be241cd5969662
Elliot Stokes <elliot.stokes@gmail.com>
2015-02-22 21:07:37 +0000

    updates

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ var app = express();
 var port = process.env.PORT || 1337
 //var reactApp = require('./react/app.jsx');
 var MyApp = React.createFactory(require('./react/app.jsx'));
-app.get('/', function(req, res) {
+
+
+app.get('/server', function(req, res) {
 	res.send(React.renderToString(MyApp()));
+});
+
+app.get('/client', function(req, res) {
+	res.file('')
 });
 
 app.listen(port);
